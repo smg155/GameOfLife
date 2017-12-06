88b0943de6901237e2e18e94ebc5117627d53f46
smg155 <smg4@illinois.edu>
2017-12-06 11:43:34 -0600

    Started working on the actual play game method.

--- GameOfLife.py
+++ GameOfLife.py
@@ -22,8 +22,14 @@ class GameBoard(object):
         a 2D array integers with the default value of zero
         for the specified size of the game board. If the size
         values for the board are greater than 100, sets the
-        size values to 100.
+        size values to 100. Does the same with time.
         '''
+        if size_x > 100:
+            size_x = 100
+        if size_y > 100:
+            size_y = 100
+        if time > 100: #Not sure about this part exactly...
+            time = 100 #Definitely have to research the time package
         self.grid = [[x for x in range(size_x)] for x in range(size_y)]
         if game_state is not None:
             self.grid = game_state
@@ -47,6 +53,20 @@ class GameBoard(object):
         '''
         The method to play the game.
         '''
+        pygame.init()
+        screen = pygame.display.set_mode((600, 600))
+        background = pygame.Surface((600, 600))
+        background.fill(black)
+        '''
+        Ask for a board size and a run time.
+        Clearly have to do this before anything can work.
+        Probably want to have a pop up box ask this and
+        have the user input their answers and what not.
+        '''
+        board = GameBoard(screen, '''size_x''', '''size_y''', '''Time''', None)
+        while True:
+            #Play da game ya
+            
 
 if __name__ == "__main__":
     play_game()
